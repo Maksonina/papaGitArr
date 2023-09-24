0750bc5c12583bdadd454450b6e71eeef92e537c
Maksonina <maksonina1@gmail.com>
2023-09-24 08:55:05 +0300

    First commit

---
+++ src/ArrStr.java
@@ -0,0 +1,7 @@
+public class ArrStr{
+    public static void main(String[] args) {
+        String g = "Иван Иванов";
+        String b = "иван иванов";
+        System.out.println(g.equals(b));
+    }
+}
