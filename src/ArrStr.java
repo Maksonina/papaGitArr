4ebc1777a4e70a9b23f7ce8831beb0ca11ee42ee
Maksonina <maksonina1@gmail.com>
2023-09-24 09:12:58 +0300

    Second commit

--- src/ArrStr.java
+++ src/ArrStr.java
@@ -2,6 +2,10 @@ public class ArrStr{
     public static void main(String[] args) {
         String g = "Иван Иванов";
         String b = "иван иванов";
-        System.out.println(g.equals(b));
+        if(g.equalsIgnoreCase(b)){
+            System.out.println("Отличное имя!");
+        }
+        else System.out.println("Выберите другое имя!");
+
     }
 }
