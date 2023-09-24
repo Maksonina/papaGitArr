f4762f43e2f5d5a7d3ecfc450783e38f38c0deee
Maksonina <maksonina1@gmail.com>
2023-09-24 09:58:34 +0300

    Fifth commit

--- src/ArrStr.java
+++ src/ArrStr.java
@@ -1,12 +1,13 @@
 public class ArrStr{
     public static void main(String[] args) {
-        String g = "иван Иванов";
-        String b = "иван иванов";
+        String g = "иван Иванов Иванович";
+        String b = "иван иванов иванович";
         checkUserName(g,b);
     }
     public static void checkUserName(String g,String b){
         if(g.equalsIgnoreCase(b)){
             System.out.println("Ваше имя имеет длину "+g.length()+" символов");
+            System.out.println("А бех пробелов Ваше имя имеет длину "+g.replace(" ","").length()+" символов");
         }
         else System.out.println("Выберите другое имя!");
 
